37cc170f72af6d80693d19a0ef131ac298d15cd4
ad101-lab <adickhans@gmail.com>
2020-04-10 19:29:14 +0000

    tried fixing the js

--- dark.js
+++ dark.js
@@ -1,6 +1,6 @@
-function w3_open() {
+/*function w3_open() {
   document.getElementById("menuSidebar").style.background-color = "black";
 }
 function w3_close() {
     document.getElementById("menuSidebar").style.display = "none";
-}
+}*/
