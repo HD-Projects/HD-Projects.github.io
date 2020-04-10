61aeba1d2a2cb257d5920e457ae63f46c55aac40
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2020-04-10 13:31:08 -0600

    Update w3.js

--- w3.js
+++ w3.js
@@ -2,5 +2,5 @@ function w3_open() {
   document.getElementById("mySidebar").style.display = "block";
 }
 function w3_close() {
-    document.getElementById("mySideber").style.display = "none";
+    document.getElementById("mySidebar").style.display = "none";
 }
