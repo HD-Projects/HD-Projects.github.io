86c58413fc994950c405437b5f974209930aa067
ad101-lab <adickhans@gmail.com>
2020-04-10 03:14:15 +0000

    made a few changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is our code for our website.
+
+[![Run on Repl.it](https://repl.it/badge/github/HD-Projects/HD-Projects.github.io)](https://repl.it/github/HD-Projects/HD-Projects.github.io)
--- dark.js
+++ dark.js
@@ -1,5 +1,5 @@
 function w3_open() {
-  document.getElementById("menuSidebar").style.display = "block";
+  document.getElementById("menuSidebar").style.background-color = "black";
 }
 function w3_close() {
     document.getElementById("menuSidebar").style.display = "none";
