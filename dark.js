de33976184ad6e5da27de0bb1e1e1aa2bd83541a
ad101-lab <adickhans@gmail.com>
2020-04-11 15:26:05 +0000

    fixed da javascript fun times

--- dark.js
+++ dark.js
@@ -1,40 +1,28 @@
+
 if(!localStorage.getItem("darkMode")) {
   localStorage.setItem("darkMode", 0);
 }
 
 var darkModeInt = parseInt(localStorage.getItem("darkMode"));
+var darkMode;
 
 if(darkModeInt == 1){
-  var darkMode = true;
-  if(darkMode){
-    document.getElementById('body').style.backgroundColor = "#000000";
-    localStorage.setItem("darkMode", 1);
-  }else{
-    document.getElementById('body').style.backgroundColor = "#ffffff";
-    localStorage.setItem("darkMode", 0);
-  }
+  darkMode = true;
+  document.getElementById('body').style.backgroundColor = "#000000";
+  document.getElementById('body').style.color = "#ffffff";
 }else{
   var darkMode = false;
-  if(darkMode){
-    document.getElementById('body').style.backgroundColor = "#000000";
-    document.getElementById('body').style.color = "#ffffff";
-    localStorage.setItem("darkMode", 1);
-  }else{
-    document.getElementById('body').style.backgroundColor = "#ffffff";
-    document.getElementById('body').style.color = "#000000";
-    localStorage.setItem("darkMode", 0);
-  }
+  document.getElementById('body').style.backgroundColor = "#ffffff";
+  document.getElementById('body').style.color = "#000000";
 }
 
 function toggle(){
   darkMode = !darkMode;
   if(darkMode){
-    document.getElementById('body').style.backgroundColor = "#000000";
-    document.getElementById('body').style.color = "#ffffff";
+    document.getElementById("body").className = "w3-dark-grey w3-container w3-center w3-border";
     localStorage.setItem("darkMode", 1);
   }else{
-    document.getElementById('body').style.backgroundColor = "#ffffff";
-    document.getElementById('body').style.color = "#000000";
+    document.getElementById("body").className = "w3-container w3-center w3-border";
     localStorage.setItem("darkMode", 0);
   }
 }
