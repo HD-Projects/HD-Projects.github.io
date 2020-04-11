b69d81d99c6c93e6f61150af495517fa6a9e5c99
ad101-lab <adickhans@gmail.com>
2020-04-11 16:26:04 +0000

    worked on js

--- dark.js
+++ dark.js
@@ -1,19 +1,19 @@
+var darkMode;
 function getDarkMode(){
   if(!localStorage.getItem("darkMode")) {
     localStorage.setItem("darkMode", 0);
   }
 
   var darkModeInt = parseInt(localStorage.getItem("darkMode"));
-  var darkMode;
 
   if(darkModeInt == 1){
     darkMode = true;
-    document.getElementById('body').style.backgroundColor = "#000000";
-    document.getElementById('body').style.color = "#ffffff";
+    document.getElementById("body").className = "w3-dark-grey w3-container w3-center w3-border";
+    document.getElementById("mySidebar").className = "w3-dark-grey w3-container w3-center w3-border";
   }else{
     var darkMode = false;
-    document.getElementById('body').style.backgroundColor = "#ffffff";
-    document.getElementById('body').style.color = "#000000";
+    document.getElementById("body").className = "w3-container w3-center w3-border";
+    document.getElementById("mySidebar").className = "w3-container w3-center w3-border";
   }
 }
 
