9f0b62db4382d9c05e773558c9dafe5d6e0e6417
ad101-lab <adickhans@gmail.com>
2020-04-12 18:47:01 +0000

    worked on dark mode

--- dark.js
+++ dark.js
@@ -21,11 +21,11 @@ function toggle(){
   darkMode = !darkMode;
   if(darkMode){
     document.getElementById("body").className = "w3-dark-grey w3-container w3-center w3-border";
-    document.getElementById("mySidebar").className = "w3-dark-grey w3-container w3-center w3-border";
+    document.getElementById("mySidebar").className = "w3-dark-grey w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
     localStorage.setItem("darkMode", 1);
   }else{
     document.getElementById("body").className = "w3-container w3-center w3-border";
-    document.getElementById("mySidebar").className = "w3-container w3-center w3-border";
+    document.getElementById("mySidebar").className = "w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
     localStorage.setItem("darkMode", 0);
   }
 }
