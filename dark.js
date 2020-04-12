5dcba11bf696fc4a5c490418101351d2e142b7e8
ad101-lab <adickhans@gmail.com>
2020-04-12 19:39:06 +0000

    JS

--- dark.js
+++ dark.js
@@ -14,11 +14,11 @@ function getDarkMode(){
   }
   if(darkMode){
     document.getElementById("body").className = "w3-black w3-container w3-center w3-border";
-    //document.getElementById("mySidebar").className = "w3-black w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
+    document.getElementById("mySidebar").className = "w3-black w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
     localStorage.setItem("darkMode", 1);
   }else{
     document.getElementById("body").className = "w3-container w3-center w3-border";
-    //document.getElementById("mySidebar").className = "w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
+    document.getElementById("mySidebar").className = "w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
     localStorage.setItem("darkMode", 0);
   }
 }
