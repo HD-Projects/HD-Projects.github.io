aed9ef541cf8105db371241b05c819a51beaa7fb
ad101-lab <adickhans@gmail.com>
2020-04-12 19:26:00 +0000

    work on the JS

--- dark.js
+++ dark.js
@@ -1,21 +1,4 @@
 var darkMode;
-function getDarkMode(){
-  if(!localStorage.getItem("darkMode")) {
-    localStorage.setItem("darkMode", 0);
-  }
-
-  var darkModeInt = parseInt(localStorage.getItem("darkMode"));
-
-  if(darkModeInt == 1){
-    darkMode = true;
-    document.getElementById("body").className = "w3-black w3-container w3-center w3-border";
-    document.getElementById("mySidebar").className = "ww3-black w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
-  }else{
-    var darkMode = false;
-    document.getElementById("body").className = "w3-container w3-center w3-border";
-    document.getElementById("mySidebar").className = "w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
-  }
-}
 
 function toggle(){
   darkMode = !darkMode;
@@ -28,4 +11,19 @@ function toggle(){
     document.getElementById("mySidebar").className = "w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
     localStorage.setItem("darkMode", 0);
   }
+}
+
+function getDarkMode(){
+  if(!localStorage.getItem("darkMode")) {
+    localStorage.setItem("darkMode", 0);
+  }
+
+  var darkModeInt = parseInt(localStorage.getItem("darkMode"));
+
+  if(darkModeInt == 1){
+    darkMode = false;
+  }else{
+    darkMode = true;
+  }
+  toggle();
 }
