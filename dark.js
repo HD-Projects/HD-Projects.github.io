d65f13e620abdca4f0454639480edebfb0deff7d
ad101-lab <adickhans@gmail.com>
2020-04-12 19:33:25 +0000

    worked on the JS

--- dark.js
+++ dark.js
@@ -1,19 +1,22 @@
 var darkMode;
 
+if(!localStorage.getItem("darkMode")) {
+  localStorage.setItem("darkMode", 0);
+}
+
 function getDarkMode(){
-  if(!localStorage.getItem("darkMode")) {
-    localStorage.setItem("darkMode", 0);
-  }
-
   var darkModeInt = parseInt(localStorage.getItem("darkMode"));
-
+  
   if(darkModeInt == 1){
     darkMode = false;
+  }else{
+    darkMode = true;
+  }
+  if(darkMode){
     document.getElementById("body").className = "w3-black w3-container w3-center w3-border";
     document.getElementById("mySidebar").className = "w3-black w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
     localStorage.setItem("darkMode", 1);
   }else{
-    darkMode = true;
     document.getElementById("body").className = "w3-container w3-center w3-border";
     document.getElementById("mySidebar").className = "w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left";
     localStorage.setItem("darkMode", 0);
