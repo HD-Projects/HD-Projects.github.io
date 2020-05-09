5ad49642d568f84aaf8ca326163c7537bb39d718
ad101-lab <adickhans@gmail.com>
2020-05-09 21:09:57 +0000

    testing

--- w3.js
+++ w3.js
@@ -1,6 +1,6 @@
 function w3_open() {
-  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("mySidebar").style.width = "100%";
 }
 function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("mySidebar").style.width = "0%";
 }
