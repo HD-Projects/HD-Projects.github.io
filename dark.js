0183d6304cf0a25832e83e527250bfd3f1460b0c
ad101-lab <adickhans@gmail.com>
2020-05-10 02:48:09 +0000

    added indev H1

--- dark.js
+++ dark.js
@@ -1,39 +1,9 @@
 var darkMode;
 
-if(!localStorage.getItem("darkMode")) {
-  localStorage.setItem("darkMode", 0);
-}
-
 function getDarkMode(){
-  var darkModeInt = parseInt(localStorage.getItem("darkMode"));
-  
-  if(darkModeInt == 1){
-    darkMode = true;
-  }else{
-    darkMode = false;
-  }
-  if(darkMode){
-    document.getElementById("body").className = "w3-black w3-container w3-center w3-border";
-    document.getElementById("mySidebar").className = "w3-black w3-sidebar w3-collapse w3-card w3-animate-left";
-    localStorage.setItem("darkMode", 1);
-  }else{
-    document.getElementById("body").className = "w3-container w3-center w3-border";
-    document.getElementById("mySidebar").className = "w3-sidebar w3-card w3-animate-left";
-    localStorage.setItem("darkMode", 0);
+  var currentURL = window.location.href;
+  if(currentURL == "https://hd-projectsgithubio-1.ad101lab.repl.co/"){
+    let top = document.getElementById("repl")
+    top.innerHTML = "<h1>This is an indev website go https://hd-projects.github.io/ to get a more stable version</h1>"
   }
 }
-
-function toggle(){
-  darkMode = !darkMode;
-  if(darkMode){
-    document.getElementById("body").className = "w3-black w3-container w3-center w3-border";
-    document.getElementById("mySidebar").className = "w3-black w3-sidebar w3-card w3-animate-left";
-    localStorage.setItem("darkMode", 1);
-  }else{
-    document.getElementById("body").className = "w3-container w3-center w3-border";
-    document.getElementById("mySidebar").className = "w3-sidebar w3-card w3-animate-left";
-    localStorage.setItem("darkMode", 0);
-  }
-}
-
-window.onload = "getDarkMode"
