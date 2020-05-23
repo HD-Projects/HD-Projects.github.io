580164fb165d7d26650f796ae8857bda0abc9b45
null null <adickhans@gmail.com>
2020-05-23 23:57:08 +0000

    worked on the JS

--- dark.js
+++ dark.js
@@ -2,7 +2,7 @@ var darkMode;
 
 function getDarkMode(){
   var currentURL = window.location.href;
-  if(currentURL == "https://hd-projectsgithubio-1.ad101lab.repl.co/"){
+  if(currentURL == "https://HD-Projectsgithubio--ad101lab.repl.co"){
     let top = document.getElementById("repl")
     top.innerHTML = "<h1>This is an indev website go https://hd-projects.github.io/ to get a more stable version</h1>"
   }
