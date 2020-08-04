b82379afb6c7d0506ae7c11a9b6bd9168f3e493e
ad101lab <adickhans@gmail.com>
2020-08-04 23:13:36 +0000

    I think I fixed the repl thing

--- dark.js
+++ dark.js
@@ -2,7 +2,7 @@ var darkMode;
 
 function getDarkMode(){
   var currentURL = window.location.href;
-  if(!(-1 == currentURL.search("repl"))){
+  if(currentURL.includes("repl")){
     let top = document.getElementById("repl")
     top.innerHTML = "<h1>This is an indev website go <a href=\"https://hd-projects.github.io/\">Here</a> to get a more stable version</h1>"
   }
