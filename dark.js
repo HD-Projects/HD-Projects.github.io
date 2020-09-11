e185369ce782fd084fd96415ced87b8740d5d577
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2020-09-11 17:39:12 -0600

    ✨made the main image on site smaller
    Co-Authored-By: The-Bit-One <61478196+The-Bit-One@users.noreply.github.com>

--- dark.js
+++ dark.js
@@ -2,8 +2,8 @@ var darkMode;
 
 function getDarkMode(){
   var currentURL = window.location.href;
-  if(currentURL.includes("repl")){
+  if(currentURL == "https://Site-CSS-redo.hdprojects.repl.co"){
     let top = document.getElementById("repl")
-    top.innerHTML = "<h1>This is an indev website go <a href=\"https://hd-projects.github.io/\">Here</a> to get a more stable version</h1>"
+    top.innerHTML = "<h1>This is an indev website go <a href=\"https://hdprojects.dev/\">Here</a> to get a more stable version</h1>"
   }
 }
